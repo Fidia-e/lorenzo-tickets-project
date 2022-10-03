079543f6278f6006642f8c2546824401b46635a7
Fidia-e <elbouanani.f@gmail.com>
2022-10-03 10:34:56 +0200

    style: 💄 adding scss structure

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/index.scss';
 
 const rootElement = document.getElementById('root');
 
--- front/src/pages/Connection/index.tsx
+++ front/src/pages/Connection/index.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import '../../styles/index.scss';
 
 const Connection: FunctionComponent = () => {
   return <div>Page de connexion</div>;
