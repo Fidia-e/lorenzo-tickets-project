001692a952df1cab8ee7b857cc5037b4850271c6
NicoExcalibur <nico.zanz@gmail.com>
2022-10-17 10:25:23 +0200

    feat: 🎸 add signin query

---
+++ back/app/helpers/jwt.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const debug = require('debug')('jwt:data');
+const { AuthenticationError } = require('apollo-server');
+
+const secret = process.env.JWT_SECRET || 'passphrase';
+
+module.exports = {
+  create(employeeData) {
+    const options = {};
+    options.expiresIn = process.env.JWT_EXPIRES;
+
+    const employee = {
+      id: employeeData.id,
+      email: employeeData.email,
+      ip: employeeData.ip,
+    };
+
+    return {
+      token: jwt.sign(employee, secret, options),
+      expiresIn: options.expiresIn,
+    };
+  },
+  get(request) {
+    if (request.header('authorization')) {
+      const bearerHeader = request.header('authorization');
+      const [token] = bearerHeader.split(' ');
+
+      try {
+        const employee = jwt.verify(token, secret);
+
+        if (!employee.ip || employee.ip !== request.ip) {
+          throw new AuthenticationError("This IP can't access to this service, please renew your token /signin");
+        }
+        debug(employee);
+
+        return employee;
+      } catch (error) {
+        throw new AuthenticationError('Token expired');
+      }
+    } else if (typeof request.header('authorization') !== 'undefined') {
+      throw new AuthenticationError('Missing token');
+    }
+    return null;
+  },
+};
