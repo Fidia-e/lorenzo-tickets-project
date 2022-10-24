50fd76af0fdc5d585661317628c13eec2ee21859
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2022-10-24 12:43:18 +0200

    test: 💍 fix connection tests

--- front/src/pages/Connection/__tests__/connection.test.tsx
+++ front/src/pages/Connection/__tests__/connection.test.tsx
@@ -1,32 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import * as RRD from 'react-router-dom';
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import '@testing-library/jest-dom';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '../../../apollo';
 
 import Connection from '../index';
-import { ButtonsClassNames, RoleText, Role } from '../constants';
+import { ButtonsClassNames, RoleText } from '../constants';
 const { selected, notSelected } = ButtonsClassNames;
 const { clientText, employeeText } = RoleText;
-const { employee, client } = Role;
 
 describe('<Connection />', () => {
-  // beforeEach(() => {
-  //   const { pathname } = useLocation();
-  //   jest.spyOn(RRD, 'useLocation').mockReturnValue({
-  //     pathname: '/'
-  //   } as unknown as any);
-
-  //   jest.mock('react-router-dom', () => ({
-  //     ...jest.requireActual('react-router-dom'),
-  //     useLocation: jest.fn().mockImplementation(() => {
-  //       return { pathname: '/' };
-  //     })
-  //   }));
-  // });
-
-  it('should always be there', () => {
+  it('should always be on screen', () => {
     render(
       <Router>
         <Routes>
@@ -34,49 +17,13 @@ describe('<Connection />', () => {
         </Routes>
       </Router>
     );
-
-    console.log(screen.debug());
 
     expect(screen.getByText('Client')).toBeInTheDocument();
     expect(screen.getByText('Employé(e)')).toBeInTheDocument();
     expect(screen.queryByLabelText('Adresse email')).toBeInTheDocument();
   });
 
-  it('should be there for client', () => {
-    React.useState = jest.fn().mockReturnValue([client, {}]);
-
-    render(
-      <Router>
-        <Routes>
-          <Route path="/" element={<Connection />} />
-        </Routes>
-      </Router>
-    );
-
-    expect(screen.getByText(clientText)).toBeInTheDocument();
-    expect(screen.queryByLabelText(employeeText)).not.toBeInTheDocument();
-    expect(screen.queryByLabelText('Mot de Passe')).not.toBeInTheDocument();
-  });
-
-  it('should be there for employee', () => {
-    React.useState = jest.fn().mockReturnValue([employee, {}]);
-
-    render(
-      <Router>
-        <Routes>
-          <Route path="/" element={<Connection />} />
-        </Routes>
-      </Router>
-    );
-
-    expect(screen.getByText(employeeText)).toBeInTheDocument();
-    expect(screen.queryByLabelText(clientText)).not.toBeInTheDocument();
-    expect(screen.queryByLabelText('Mot de Passe')).toBeInTheDocument();
-  });
-
-  it('should have the client button selected', () => {
-    React.useState = jest.fn().mockReturnValue([client, {}]);
-
+  it('is a client', () => {
     render(
       <Router>
         <Routes>
@@ -88,6 +35,10 @@ describe('<Connection />', () => {
     const buttons = screen.getAllByRole('button');
     const clientButton = buttons[0];
     const employeeButton = buttons[1];
+
+    expect(screen.getByText(clientText)).toBeInTheDocument();
+    expect(screen.queryByLabelText(employeeText)).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Mot de Passe')).not.toBeInTheDocument();
 
     expect(clientButton).toHaveClass(selected);
     expect(clientButton).not.toHaveClass(notSelected);
@@ -95,20 +46,26 @@ describe('<Connection />', () => {
     expect(employeeButton).toHaveClass(notSelected);
   });
 
-  it('should have the employee button selected', () => {
-    React.useState = jest.fn().mockReturnValue([employee, {}]);
-
+  it('is an employee', () => {
     render(
-      <Router>
-        <Routes>
-          <Route path="/" element={<Connection />} />
-        </Routes>
-      </Router>
+      <ApolloProvider client={apolloClient}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Connection />} />
+          </Routes>
+        </Router>
+      </ApolloProvider>
     );
 
     const buttons = screen.getAllByRole('button');
     const clientButton = buttons[0];
     const employeeButton = buttons[1];
+
+    fireEvent.click(employeeButton);
+
+    expect(screen.getByText(employeeText)).toBeInTheDocument();
+    expect(screen.queryByLabelText(clientText)).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Mot de Passe')).toBeInTheDocument();
 
     expect(employeeButton).toHaveClass(selected);
     expect(employeeButton).not.toHaveClass(notSelected);
