ef87af7c88b1e21c6155ee9fa3df9cb946b8868b
NicoExcalibur <nico.zanz@gmail.com>
2022-10-25 12:14:41 +0200

    feat: 🎸 Add schema types generation
    fix query ticket

---
+++ front/__generated__/globalTypes.ts
@@ -0,0 +1,29 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum Role {
+  admin = "admin",
+  intervenor = "intervenor",
+  lead = "lead",
+}
+
+export enum Status {
+  closed = "closed",
+  open = "open",
+  underway = "underway",
+}
+
+export enum UserType {
+  client = "client",
+  employee = "employee",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
