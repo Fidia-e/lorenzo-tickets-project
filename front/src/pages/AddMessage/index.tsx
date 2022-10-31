b12eb06319198e16cb2b07409b59d0c83542ab8b
Fidia-e <elbouanani.f@gmail.com>
2022-10-31 15:00:56 +0100

    style: 💄 retrait imports scss inutiles

--- front/src/pages/AddMessage/index.tsx
+++ front/src/pages/AddMessage/index.tsx
@@ -1,5 +1,4 @@
 import { FunctionComponent } from 'react';
-import '../../styles/index.scss';
 
 const AddMessage: FunctionComponent = () => {
   return <h1>Page d&apos;ajout d&apos;un message</h1>;
